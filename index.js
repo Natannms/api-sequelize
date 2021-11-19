4673fead1d79a5190e84f92e86ae2a9628829509
Lara Graziela <laragdiogo@gmail.com>
2021-11-19 16:28:04 -0300

    Criação das rotas index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { restart } = require('nodemon');
 const app = express();
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + "/src/pages/index.html")
-})
+});
 
+app.get('/login', (req, res)=>{
+    res.sendFile(_dirname + "/src/pages/login.html")
+});
+
+app.get('/register', (req, res)=>{
+    res.sendFile(_dirname + "/src/pages/register.html")
+});
+
+app.get('/resetPass', (req, res)=>{
+    res.sendFile(_dirname + "/src/pages/resetpass.html")
+});
 
 app.listen(3000, function(){
     console.log('App listen in pornt 300 => http://localhost:3000')
